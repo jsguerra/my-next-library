eaffa4f112f09fada9e6ced3501d6608a9e671e5
jsguerra <jose@gcostudios.com>
2023-11-18 12:38:43 +0000

    file updated

--- src/app/authors/[slug]/page.tsx
+++ src/app/authors/[slug]/page.tsx
@@ -1,7 +1,10 @@
+import Link from "next/link";
+
 export default function Author() {
   return (
     <div className="container">
       <h1>Author ID: </h1>
+      <Link className="btn" href="/authors/edit/lorem-ipsum">Edit Author</Link>
     </div>
   );
 }
--- src/app/authors/page.tsx
+++ src/app/authors/page.tsx
@@ -1,7 +1,11 @@
+import Link from "next/link";
+
 export default function AuthorsDirecotry() {
   return (
     <div className="container">
       <h1>Directory of Authors</h1>
+      <Link className="btn" href="/authors/add">Add Author</Link>
+      <Link className="btn" href="/authors/lorem-ipsum">Lorem Ispum</Link>
     </div>
   );
 }
