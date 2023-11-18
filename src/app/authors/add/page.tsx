c0b9a110131c29f0723a6885c504ff44fdecc517
jsguerra <jose@gcostudios.com>
2023-11-18 14:38:08 +0000

    updated markup

--- src/app/authors/add/page.tsx
+++ src/app/authors/add/page.tsx
@@ -1,7 +1,24 @@
-export default function AddAuthor() {
+import prisma from "@/lib/prisma";
+import AuthorForm from "@/components/forms/AuthorForm/AuthorForm";
+
+export default async function AddAuthor() {
+  const authors = await prisma.author.findMany();
+
   return (
-    <div className="container">
-      <h1>Add of Author</h1>
+    <div style={{ maxWidth: "800px" }} className="container">
+      <h1>Create Author</h1>
+      <p>Add your author details.</p>
+      <AuthorForm />
+      {authors.length > 0 && (
+        <>
+          <h2>List of Authors</h2>
+          <ul>
+            {authors.map((author) => (
+              <li key={author.id}>{author.name}</li>
+            ))}
+          </ul>
+        </>
+      )}
     </div>
   );
 }
