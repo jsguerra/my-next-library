f4bbe7160c75bcc66338489921c11c450a452589
jsguerra <jose@gcostudios.com>
2023-11-18 15:32:02 +0000

    added logic and fetch data

--- src/app/authors/[slug]/page.tsx
+++ src/app/authors/[slug]/page.tsx
@@ -1,11 +1,41 @@
+import Grid from "@/components/Grid/Grid";
+import prisma from "@/lib/prisma";
 import Link from "next/link";
 
-export default function Author() {
+export default async function Author({ params }: { params: { slug: string } }) {
+  const authors = await prisma.author.findMany({
+    where: {
+      slug: params.slug,
+    },
+    include: {
+      books: true,
+    },
+  });
+
+  const author = authors[0];
+
   return (
-    <div className="container">
-      <h1>Author ID: </h1>
-      <Link className="btn" href="/authors/edit/lorem-ipsum">Edit Author</Link>
-      <Link className="btn" href="/book/i-robot">View Book</Link>
-    </div>
+    <>
+      <div className="container">
+        <h1>Author: {author.name}</h1>
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "space-between",
+            alignItems: "center",
+          }}
+        >
+          <p>Total books by author: {author.books.length}</p>
+          <Link className="btn" href="/authors/edit/lorem-ipsum">
+            Edit Author
+          </Link>
+        </div>
+      </div>
+      <Grid numOfCol={author.books.length > 0 ? "5" : "1"}>
+        {author.books.length > 0 ? "" : <p>No books by author yet</p>}
+        {author.books &&
+          author.books.map((book) => <p key={book.id}>{book.title}</p>)}
+      </Grid>
+    </>
   );
 }
--- src/app/authors/page.tsx
+++ src/app/authors/page.tsx
@@ -1,11 +1,44 @@
 import Link from "next/link";
+import prisma from "@/lib/prisma";
+import Grid from "@/components/Grid/Grid";
 
-export default function AuthorsDirecotry() {
+export default async function AuthorsDirecotry() {
+  const authors = await prisma.author.findMany();
+
   return (
     <div className="container">
       <h1>Directory of Authors</h1>
-      <Link className="btn" href="/authors/add">Add Author</Link>
-      <Link className="btn" href="/authors/lorem-ipsum">Lorem Ispum</Link>
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "space-between",
+          alignItems: "center",
+        }}
+      >
+        <p>Total authors: {authors.length}</p>
+        <Link className="btn" href="/authors/add">
+          Add Author
+        </Link>
+      </div>
+      <p>Filter by letter</p>
+
+      <Grid numOfCol={authors.length > 0 ? "5" : "1"}>
+        {authors.length > 0 ? "" : <p>No authors in My Next Library yet</p>}
+        {authors &&
+          authors.map((author) => {
+            const directory = author.slug.slice(0, 1);
+
+            return (
+              <Link key={author.id} href={`/authors/${author.slug}`}>
+                <img
+                  src={`/library/${directory}/${author.slug}/${author.thumbnail}`}
+                  alt={author.name}
+                />
+                {author.name}
+              </Link>
+            );
+          })}
+      </Grid>
     </div>
   );
 }
