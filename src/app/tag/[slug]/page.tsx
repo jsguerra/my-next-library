68a62c0d804354c15799a31fee4f6a400df5d596
jsguerra <jose@gcostudios.com>
2023-11-20 08:08:24 +0000

    updated page logic

--- src/app/tag/[slug]/page.tsx
+++ src/app/tag/[slug]/page.tsx
@@ -1,10 +1,53 @@
 import Link from "next/link";
+import prisma from "@/lib/prisma";
+import Grid from "@/components/Grid/Grid";
+import Styles from "./page.module.css";
 
-export default function Tag() {
+export default async function Tag({ params }: { params: { slug: string } }) {
+  const tags = await prisma.tag.findMany({
+    where: {
+      slug: params.slug,
+    },
+    include: {
+      books: {
+        include: {
+          author: true,
+        },
+      },
+    },
+  });
+
   return (
-    <div className="container">
-      <h1>Tag ID: </h1>
-      <Link className="btn" href="/book/edit/sci-fi">Edit Tag</Link>
-    </div>
+    <>
+      <div className="container">
+        <h1>Tag: {tags[0].name}</h1>
+        <div className={Styles.meta}>
+          <p>Total books: {tags.length}</p>
+          <p>Page: 1 of 1</p>
+        </div>
+      </div>
+      <Grid numOfCol={tags[0].books.length > 0 ? "5" : "1"}>
+        {tags[0].books.length > 0 ? "" : <p>No books in this tag yet</p>}
+        {tags[0].books.length > 0 &&
+          tags.map((tag, i) => {
+            const authorDirectory = tag.books[i].author.slug;
+            const firstLetter = authorDirectory.slice(0, 1);
+            const pages = tag.books[i].pages?.split(", ");
+
+            return (
+              <div style={{ textAlign: "center" }} key={tag.books[i].id}>
+                {pages && (
+                  <Link href={`/book/${tag.books[i].slug}`}>
+                    <img
+                      src={`/library/${firstLetter}/${authorDirectory}/${tag.books[i].slug}/${pages[0]}`}
+                    />
+                  </Link>
+                )}
+                <p>{tag.books[i].title}</p>
+              </div>
+            );
+          })}
+      </Grid>
+    </>
   );
 }
