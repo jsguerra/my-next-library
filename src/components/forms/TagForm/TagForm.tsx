78d7b433cb47d89d01f68b262fa1b0a45f5d7272
jsguerra <jose@gcostudios.com>
2023-11-22 13:16:01 +0000

    updated form with default values

--- src/components/forms/TagForm/TagForm.tsx
+++ src/components/forms/TagForm/TagForm.tsx
@@ -4,7 +4,14 @@ import { useRef } from "react";
 import { tagAction } from "@/actions/tagActions";
 import Styles from "./TagForm.module.css";
 
-export default function TagForm() {
+interface TagFormProps {
+  data?: {
+    name: string;
+    slug: string;
+  }
+}
+
+export default function TagForm({ data }: TagFormProps) {
   const ref = useRef<HTMLFormElement>(null);
 
   return (
@@ -13,24 +20,30 @@ export default function TagForm() {
       action={async (formData) => {
         ref.current?.reset();
 
-        await tagAction(formData);
+        if (!data) {
+          await tagAction(formData);
+        }
+
+        // Add edit action here
       }}
       className={Styles.form}
     >
       <input
         name="name"
-        placeholder="Tag name"
+        placeholder="Name"
         type="text"
         autoComplete="off"
+        defaultValue={data?.name}
       />
       <input
         name="slug"
-        placeholder="Tag slug"
+        placeholder="Slug"
         type="text"
         autoComplete="off"
+        defaultValue={data?.slug}
       />
       <button className="btn" type="submit">
-        Add Tag
+        {data ? "Edit Tag" : "Add Tag"}
       </button>
     </form>
   );
