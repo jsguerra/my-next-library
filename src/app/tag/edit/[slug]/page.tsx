f7bae51595b3fb3e355459a88f0949a12b8816fc
jsguerra <jose@gcostudios.com>
2023-11-22 13:16:14 +0000

    page updated

--- src/app/tag/edit/[slug]/page.tsx
+++ src/app/tag/edit/[slug]/page.tsx
@@ -1,7 +1,22 @@
-export default function EditTag() {
+import prisma from "@/lib/prisma";
+import TagForm from "@/components/forms/TagForm/TagForm";
+
+export default async function EditTag({
+  params,
+}: {
+  params: { slug: string };
+}) {
+  const tags = await prisma.tag.findMany({
+    where: {
+      slug: params.slug,
+    },
+  });
+
   return (
-    <div className="container">
-      <h1>Edit Tag ID: </h1>
+    <div style={{ maxWidth: "300px"}} className="container">
+      <h1>Edit {tags[0].name}</h1>
+      <p>Update details below:</p>
+      <TagForm data={tags[0]} />
     </div>
   );
 }
