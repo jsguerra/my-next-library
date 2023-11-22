02cdf96da2c8b1b65c34dc7a5b8cac00ad6bcd0c
jsguerra <jose@gcostudios.com>
2023-11-22 09:24:31 +0000

    added button column toggles

--- src/components/Grid/Grid.tsx
+++ src/components/Grid/Grid.tsx
@@ -1,15 +1,37 @@
-import React from "react";
+"use client";
+
+import React, { useState } from "react";
 import styles from "./Grid.module.css";
 
 type GridProps = {
-  numOfCol: string;
+  gridColToggle?: boolean;
   children: React.ReactNode;
 };
 
-export default function Grid({ numOfCol, children }: GridProps) {
+export default function Grid({ gridColToggle = false, children }: GridProps) {
+  const [numOfCol, setNumOfCol] = useState(5);
+  const colNums = [5, 4, 3, 2, 1];
+
   return (
-    <div className={`${styles.grid} ${styles[`columns-${numOfCol}`]}`}>
-      {children}
+    <div>
+      {gridColToggle && (
+        <div className={styles["grid-btn-container"]}>
+          <span>Toggle Grid Columns: </span>
+          {colNums.map((btn) => (
+            <button
+              className="btn"
+              key={`btn-${btn}`}
+              onClick={() => setNumOfCol(btn)}
+              type="button"
+            >
+              {btn}
+            </button>
+          ))}
+        </div>
+      )}
+      <div className={`${styles.grid} ${styles[`columns-${numOfCol}`]}`}>
+        {children}
+      </div>
     </div>
   );
 }
