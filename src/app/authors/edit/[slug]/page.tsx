7db15fa0177962cfb71325447ec2250276eaa9e6
jsguerra <jose@gcostudios.com>
2023-11-23 08:46:35 +0000

    added temporary markup

--- src/app/authors/edit/[slug]/page.tsx
+++ src/app/authors/edit/[slug]/page.tsx
@@ -1,7 +1,23 @@
-export default function EditAuthor() {
+import prisma from "@/lib/prisma";
+
+export default async function EditAuthor({
+  params,
+}: {
+  params: { slug: string };
+}) {
+  const author = await prisma.author.findUnique({
+    where: {
+      id: Number(params.slug),
+    },
+  });
+
   return (
-    <div className="container">
-      <h1>Edit Author ID: </h1>
+    <div
+      style={{ color: "#E3A008", maxWidth: "800px", textAlign: "center" }}
+      className="container"
+    >
+      <h1 style={{ fontSize: "2.5rem" }}>Under Contrustion</h1>
+      <p>This feature is not available yet.</p>
     </div>
   );
 }
