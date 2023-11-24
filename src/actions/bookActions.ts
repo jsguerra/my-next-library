cc2dbdcc1d867994453f794b206f3fb8bd779b58
jsguerra <jose@gcostudios.com>
2023-11-24 00:06:29 +0000

    edit action added

--- src/actions/bookActions.ts
+++ src/actions/bookActions.ts
@@ -84,6 +84,82 @@ export async function bookAction(data: FormData) {
   return { success: true };
 }
 
-export async function bookEditAction(data: FormData) {}
+export async function bookEditAction(data: FormData) {
+  const id = data.get("id");
+  const title = data.get("title");
+  const slug = data.get("slug");
+  const authorId = data.get("authorId");
+  const favIsOn = data.get("favorite");
+  const tags = data.getAll("tag");
+
+  const tagIdsArray: number[] = Array.from(tags).reduce(
+    (acc: number[], tagId) => {
+      if (typeof tagId === "string") {
+        acc.push(parseInt(tagId, 10));
+      }
+      return acc;
+    },
+    []
+  );
+
+  console.log(tagIdsArray);
+
+  const favorite = favIsOn === "on" ? true : false;
+
+  const author = await prisma.author.findMany({
+    where: {
+      id: Number(authorId),
+    },
+  });
+
+  console.log(data);
+
+  const firstLetter = author[0].slug.slice(0, 1);
+  const myPath = `public/library/${firstLetter}/${author[0].slug}/${slug}`;
+
+  const files: FileList | null = data.getAll("images") as unknown as FileList;
+
+  let pages = "";
+
+  if (!files) {
+    throw new Error("No file uploaded");
+  }
+
+  for (let i = 0; i < files.length; i++) {
+    const file = files[i];
+    const bytes = await file.arrayBuffer();
+    const buffer = Buffer.from(bytes);
+
+    i > 0 ? (pages += ", " + file.name) : (pages += file.name);
+
+    if (!fs.existsSync(myPath)) {
+      fs.mkdirSync(myPath, { recursive: true });
+    }
+
+    const path = join("./", myPath, file.name);
+    await writeFile(path, buffer);
+    console.log(`open ${path} to see the uploaded file`);
+  }
+
+    // Add entry into database
+    await prisma.book.update({
+      where: {
+        id: Number(id)
+      },
+      data: {
+        title: title as string,
+        pages: pages as string,
+        favorite: favorite,
+        tag: {
+          connect: tagIdsArray.map((tagId) => ({ id: tagId })),
+        },
+        authorId: author[0].id
+      },
+    });
+  
+    revalidatePath("/");
+  
+    return { success: true };
+}
 
 export async function bookDeleteAction(data: FormData) {}
