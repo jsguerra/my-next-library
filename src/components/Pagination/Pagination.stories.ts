358e559b6052a2746b00725d3ee60e5515468604
jsguerra <jose@gcostudios.com>
2023-12-02 23:44:50 +0000

    updated stories and centered

--- src/components/Pagination/Pagination.stories.ts
+++ src/components/Pagination/Pagination.stories.ts
@@ -4,11 +4,28 @@ import Pagination from "./Pagination";
 const meta = {
   title: "atom/Pagination",
   component: Pagination,
+  parameters: {
+    layout: "centered",
+  },
 } satisfies Meta<typeof Pagination>;
 
 export default meta;
 type Story = StoryObj<typeof meta>;
 
 export const Default: Story = {
-  args: {},
+  args: {
+    pages: [1, 2, 3, 4, 5],
+  },
+};
+
+export const Short: Story = {
+  args: {
+    pages: [1, 2, 3],
+  },
+};
+
+export const Long: Story = {
+  args: {
+    pages: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15],
+  },
 };
