56f67f88eb59a54fe5d4fc57d0696e937e540468
jsguerra <jose@gcostudios.com>
2023-12-03 16:53:12 +0000

    added pagination logic

--- src/components/Pagination/Pagination.tsx
+++ src/components/Pagination/Pagination.tsx
@@ -1,43 +1,61 @@
 import Link from "next/link";
 import Styles from "./Pagination.module.css";
 
-type PaginationProps = {
-  pages: number[];
-};
+interface PaginationProps {
+  page: number;
+  pages: number;
+}
 
-const Pagination: React.FC<PaginationProps> = ({ pages }) => {
+const Pagination: React.FC<PaginationProps> = ({ page, pages }) => {
+  const buttons = [];
+
+  for (let i = 1; i <= pages; i++) {
+    if (
+      i === page ||
+      i === page + 1 ||
+      i === page + 2 ||
+      i === page + 3 ||
+      i === page + 4
+    ) {
+      buttons.push(
+        <li key={`page-${i + 1}`}>
+          <Link href={`/?page=${i}`}>{i}</Link>
+        </li>
+      );
+    }
+  }
+
   return (
     <nav className={Styles.pagination}>
       <ul>
-        {pages.length > 5 && (
-          <li>
-            <Link className={Styles.previous} href={"/"}>
-              <span className="sr-only">Previous</span>
-              <svg
-                aria-hidden="true"
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 6 10"
-              >
-                <path
-                  stroke="currentColor"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  stroke-width="2"
-                  d="M5 1 1 5l4 4"
-                />
-              </svg>
-            </Link>
-          </li>
-        )}
-        {pages.length > 0 &&
-          pages.map((page, i) => (
-            <li key={i}>
-              <Link href={`/?page=${page}`}>{page}</Link>
-            </li>
-          ))}
         <li>
-          <Link className={Styles.next} href={"/"}>
+          <Link
+            className={Styles.previous}
+            href={`/?page=${page === 1 ? page : page - 1}`}
+          >
+            <span className="sr-only">Previous</span>
+            <svg
+              aria-hidden="true"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 6 10"
+            >
+              <path
+                stroke="currentColor"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M5 1 1 5l4 4"
+              />
+            </svg>
+          </Link>
+        </li>
+        {buttons}
+        <li>
+          <Link
+            className={Styles.next}
+            href={`/?page=${page >= pages ? page : page + 1}`}
+          >
             <span className="sr-only">Next</span>
             <svg
               aria-hidden="true"
@@ -47,9 +65,9 @@ const Pagination: React.FC<PaginationProps> = ({ pages }) => {
             >
               <path
                 stroke="currentColor"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
                 d="m1 9 4-4-4-4"
               />
             </svg>
